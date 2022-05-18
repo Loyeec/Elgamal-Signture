8bd4615fc709948196e558d76492683ec4f8ca86
Loyeec <1601814489@qq.com>
2022-05-18 23:21:44 +0800

    2020
    Cuit Ganster

---
+++ 2/Server/CS_Server.java
@@ -0,0 +1,151 @@
+package Server;
+import java.net.*;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+public class CS_Server 
+
+{
+	public static void main(String[] args) throws Exception
+	{
+		ServerSocket ss = new ServerSocket(38380);//是一个能够接受其他通信实体请求的类
+		System.out.println("服务器正在等待客户端的连接请求----");
+		//用一个while循环可以同时响应多个客户端的请求
+		while(true){
+			 Socket sk= ss.accept();//服务器监听对应端口的输入
+			 ServerThread  st = new ServerThread(sk);//创建一个线程，用线程创建一个套接字
+			 st.start();
+		}
+	}
+}
+
+//服务器线程类
+class ServerThread extends Thread
+{
+	static BigInteger p, g; // 大素数和本原元
+	static BigInteger C, C1;// 密文
+	Socket sk;
+	public ServerThread(Socket sk){
+	this.sk= sk;
+	}
+	public void run() {		
+		BigInteger y, x; // 随机数 P,g是P的生成元，公钥<y,g,p>，私钥<x,g,p> 0<a<p
+		int z=32;
+		//p=BigInteger.valueOf(7);
+		//g=BigInteger.valueOf(5);
+		
+		BufferedReader br=null;
+		try{
+		br  = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+		String line = br.readLine();
+		System.out.println("来自客户端的数据："+line);
+		int hm = line.hashCode();//明文的哈希值
+			System.out.println("该明文的哈希值为："+hm);
+ 
+			/*int hm = message.hashCode();//明文的哈希值
+			System.out.println("该明文的哈希值为："+hm);*/
+ 
+			ServerThread.getRandomP(z);
+			x = ServerThread.getRandoma(p);
+			y = ServerThread.calculatey(x, g, p);
+			System.out.println("计算机随机生成的素数P:" + p);
+			System.out.println("求得其生成元g:" + g);
+			System.out.println("私钥<x,g,p>为: (" + x + "," + g + "," + p + ")");
+			System.out.println("公钥<y,g,p>为:" + "(" + y + "," + g + "," + p
+					+ ")");
+			
+			ServerThread.encrypt(hm, x, p, g);
+			ServerThread.decrypt(C, C1, hm,g, p,y);
+			
+
+		br.close();
+		sk.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+
+	/** 取一个大的随机素数P,和P的生成元g */
+	public static void getRandomP(int z) {
+		Random r = new Random();
+		BigInteger q = null;
+		while (true) {
+			q = BigInteger.probablePrime(z, r);
+			if (q.bitLength() !=z)
+				continue;
+			if (q.isProbablePrime(10)) // 如果q为素数则再进一步计算生成元
+			{
+				p = q.multiply(new BigInteger("2")).add(BigInteger.ONE);
+				if (p.isProbablePrime(10)) // 如果P为素数则OK~，否则继续
+					break;
+			}
+		}
+		while (true) {
+			g = BigInteger.probablePrime(p.bitLength() - 1, r);// 产生一0<=k<p-1的随机素数
+			if (!g.modPow(BigInteger.ONE, p).equals(BigInteger.ONE)
+					&& !g.modPow(q, p).equals(BigInteger.ONE)) {
+				break;
+			}
+		}
+	}
+ 
+	/** 取随机数a */
+	public static BigInteger getRandoma(BigInteger p) {
+		BigInteger a;
+		Random r = new Random();
+		a = new BigInteger(p.bitLength() - 1, r);// 产生一0<=a<p-1的随机数
+		System.out.println("随机数 x 为:" + a);
+		return a;
+	}
+ 
+	/** 计算密钥的值 */
+	public static BigInteger calculatey(BigInteger x, BigInteger g, BigInteger p) {
+		BigInteger y;
+		y = g.modPow(x, p);//y=g^a mod p
+		return y;
+	}
+ 
+	/** 签名 */
+	public static void encrypt(int m, BigInteger x, BigInteger p,
+			BigInteger g) {
+//		BigInteger message = new BigInteger(m.getBytes());// 把字串转成一个BigInteger对象
+		BigInteger message=BigInteger.valueOf(m);
+		Random r = new Random();
+		BigInteger k;
+		while (true) {
+			k = new BigInteger(p.bitLength() - 2, r);// 产生一0<=k<p-2的随机数
+			if (k.gcd(p.subtract(BigInteger.ONE)).equals(BigInteger.ONE)) {// 如果随机数与p-1互质																			// 则选取成功,返回随机数k
+				System.out.println("随机数 k 为:"+k);
+				break;
+			}
+		}
+		// 计算签名C,C1
+		C = g.modPow(k, p);//c=g^k mod p
+		BigInteger p_MinusOne=p.subtract(BigInteger.ONE);
+		BigInteger k_Reverse = k.modInverse(p_MinusOne);
+        System.out.println("k对p-1的逆元 = " + k_Reverse.toString());
+		C1=k_Reverse.multiply(message.subtract(x.multiply(C))).mod(p_MinusOne);
+		System.out.println("签名C:"+C);
+		System.out.println("签名C1:"+C1);
+		
+	}
+ 
+	/** 认证 */
+	public static void decrypt(BigInteger C, BigInteger C1, int hm,BigInteger g,
+			BigInteger p,BigInteger y) {
+		
+		BigInteger m=BigInteger.valueOf(hm);
+		BigInteger v1,v2;
+		
+		v1=g.modPow(m,p);
+		v2=((y.modPow(C,p)).multiply(C.modPow(C1,p))).mod(p);
+		System.out.println("认证v1:"+v1);
+		System.out.println("认证v2:"+v2);
+ 
+	}
+}
+
---
+++ 2/zz.md
@@ -0,0 +1,5 @@
+```
+https://blog.csdn.net/weixin_48097919/article/details/121591236?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522165270408116781683933307%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=165270408116781683933307&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_ecpm_v1~rank_v31_ecpm-22-121591236-null-null.142^v9^pc_search_result_control_group,157^v4^control&utm_term=eigamal%E6%95%B0%E5%AD%97%E7%AD%BE%E5%90%8D%E5%AE%9E%E7%8E%B0java&spm=1018.2226.3001.4187
+```
+
+![image-20220516205739221](C:\Users\GL65\AppData\Roaming\Typora\typora-user-images\image-20220516205739221.png)
